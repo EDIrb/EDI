ecea8204eebe7173b03dc0d9e2f91cbdc29f911e
Bradley Temple <bradley.temple@jpmchase.com>
2015-02-17 15:20:14 -0500

    Clean up specs

--- spec/jarvis/service_spec.rb
+++ spec/jarvis/service_spec.rb
@@ -2,14 +2,17 @@ require 'spec_helper'
 
 RSpec.describe Jarvis::Service do
 
-  describe "Validate Environment Variables are Present" do
-
+  before(:all) do
     class MyService < Jarvis::Service
       required_environment "SERVICE_TOKEN"
     end
+    class MyOtherService < Jarvis::Service
+      required_environment "SERVICE_TOKEN", "SERVICE_SECRET"
+    end
+  end
 
+  describe "Validate Environment Variables are Present" do
     subject { MyService.new }
-
     context "Required Environment not set up" do
       it { expect { subject.validate_environment }.to raise_exception(Jarvis::UnfitEnvironmentException) }
     end
@@ -17,17 +20,22 @@ RSpec.describe Jarvis::Service do
     context "Environment Correctly Set Up" do
       before { ENV["SERVICE_TOKEN"] = "token" }
       it { expect { subject.validate_environment }.to_not raise_exception }
+      after { ENV["SERVICE_TOKEN"] = nil }
     end
 
     context "Multiple Required Variables, Some not present" do
-      class MyOtherService < Jarvis::Service
-        required_environment "SERVICE_TOKEN", "SERVICE_SECRET"
-      end
       subject { MyOtherService.new }
       before { ENV["SERVICE_TOKEN"] = "token" }
       it { expect { subject.validate_environment }.to raise_exception(Jarvis::UnfitEnvironmentException) }
+      after { ENV["SERVICE_TOKEN"] = nil }
     end
+  end
 
+
+  after(:all) do
+    # Clean Up Class Definitions
+    Object.send(:remove_const, :MyService)
+    Object.send(:remove_const, :MyOtherService)
   end
 
 end
