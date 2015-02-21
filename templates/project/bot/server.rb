b70e6ccd3184b96b6638235839649b038e3f84d3
DVG <devryguy@gmail.com>
2015-02-20 22:18:22 -0500

    Updated server template

--- templates/project/bot/server.rb
+++ templates/project/bot/server.rb
@@ -1,6 +1,6 @@
 class Server < Jarvis::Server
   # Use `register_services` to enable services in your Jarvis Bot. Available built-in services are:
-  # :dice, :eightball, :fact, :giphy
+  # :dice, :eightball, :fact, :giphy, :i_heart_quotes
   # You can make your own services with `jarvis generate service my_service`
   register_services :fact
 end
