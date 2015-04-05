7fc19fd3328115c807f2119258c3f713a99878d6
DVG <devryguy@gmail.com>
2015-04-04 21:51:16 -0400

    Youtube Service

---
+++ lib/edi/services/youtube.rb
@@ -0,0 +1,42 @@
+class Youtube < EDI::Service
+  phrases "youtube"
+
+  environment :google_api_key
+  before_invoke :extract_search_term
+
+  def run
+    response = EDI.get "#{url}?#{params}"
+    "https://youtu.be/#{response["items"].first["id"]["videoId"]}"
+  end
+
+private
+
+  def extract_search_term
+    captures = text.match /youtube(?<term>.+)/i
+    @search_term = captures[:term]
+  end
+
+  def url
+    "https://www.googleapis.com/youtube/v3/search"
+  end
+
+  def params
+    [part, order, q, key].join("&")
+  end
+
+  def part
+    "part=snippet"
+  end
+
+  def order
+    "order=rating"
+  end
+
+  def key
+    "key=#{google_api_key}"
+  end
+
+  def q
+    "q=#{@search_term}"
+  end
+end
