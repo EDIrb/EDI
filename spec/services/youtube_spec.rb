80bd12313e937a56955c3e6ffeec1a98855560fc
DVG <devryguy@gmail.com>
2015-04-06 11:55:59 -0400

    Youtube spec fix

--- spec/services/youtube_spec.rb
+++ spec/services/youtube_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
-RSpec.describe Youtube, vcr: { cassette_name: 'youtube' } do
+RSpec.describe Youtube, vcr: { cassette_name: 'youtube', record: :new_episodes } do
   let(:message) { Slack::Message.new(slack_outgoing_message(text: "EDI, youtube Harry Potter and the Half Blood Prince")) }
   subject { described_class.new message }
+  before { stub_env "GOOGLE_API_KEY" => "some_token" }
   it { expect(subject.invoke).to match /https:\/\/youtu.be/ }
 end
